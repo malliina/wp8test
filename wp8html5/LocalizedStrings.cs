8b3245dfca15d92952abe8c531d4af81c5960dc7
Michael Skogberg <malliina123@gmail.com>
2012-12-11 23:14:21 +0200

    init

---
+++ wp8html5/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using wp8html5.Resources;
+
+namespace wp8html5
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
